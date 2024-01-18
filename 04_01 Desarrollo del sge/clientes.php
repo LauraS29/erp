39fbaeffc972038e9b29e4b710d6289e421b0c03
LauraS29 <lasalmo03@gmail.com>
2024-01-18 11:02:57 +0100

    .

--- 04_01 Desarrollo del sge/clientes.php
+++ 04_01 Desarrollo del sge/clientes.php
@@ -16,7 +16,7 @@
             <a href="#">Productos</a><br>
             <a href="#">Ventas</a><br>
             <a href="#">Compra</a><br>
-            <a href="#">Pedidos</a><br>
+            <a href="#">Pedidoss</a><br>
         </div>
     </header>
     <section>
