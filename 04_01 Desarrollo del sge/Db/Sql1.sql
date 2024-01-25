7f4a181745da1b580fd48cee565318c5be473d65
LauraS29 <lasalmo03@gmail.com>
2024-01-25 13:27:30 +0100

    .

--- 04_01 Desarrollo del sge/Db/Sql1.sql
+++ 04_01 Desarrollo del sge/Db/Sql1.sql
@@ -2,20 +2,19 @@ CREATE DATABASE IF NOT EXISTS trabajo;
 USE trabajo;
 
 CREATE TABLE Proveedores (
-    Cod_producto INT PRIMARY KEY,
-    Cod_proveedor INT,
-   
+    Cod_proveedor INT AUTOINCREMENT,
     Nom_proveedor VARCHAR(25),
     Email_proveedor VARCHAR(25),
     Fecha_envio DATE
+    FOREIGN KEY (Cod_producto) REFERENCES Producto(Cod_producto)
 );
 
 CREATE TABLE Cliente (
-    CP INT PRIMARY KEY,
     Cod_cliente INT AUTOINCREMENT,
     DNI_cliente VARCHAR(10),
     Nom_cliente VARCHAR(25),
     Ap_cliente VARCHAR(25),
+    Cod_postal VARCHAR(25),
     Localidad VARCHAR(25),
     Provincia VARCHAR(25),
     Email_cliente VARCHAR(25),
@@ -24,41 +23,29 @@ CREATE TABLE Cliente (
 );
 
 CREATE TABLE Producto (
-    CP INT PRIMARY KEY,
+    Cod_producto INT PRIMARY KEY,
     Precio DECIMAL(10, 2),
-
-);
-
-CREATE TABLE Empresa (
-    CP_Proveedor INT, 
-    Nom_empresa VARCHAR(25),
-    CP_Cliente INT,
-    FOREIGN KEY (CP_Proveedor) REFERENCES Proveedores(CP),
-    FOREIGN KEY (CP_Cliente) REFERENCES Cliente(CP),
-    PRIMARY KEY (CP_Proveedor, CP_Cliente)
+    Cantidad INT
 );
 
 CREATE TABLE Tienda (
-    CP_Cliente INT,
-    CP_Producto INT,
-    CP_Empleado INT,
     N_Stock INT,
-    FOREIGN KEY (CP_Cliente) REFERENCES Cliente(CP),
-    FOREIGN KEY (CP_Producto) REFERENCES Producto(CP),
-    FOREIGN KEY (CP_Empleado) REFERENCES Empleados(CP),
-    PRIMARY KEY (CP_Cliente, CP_Producto, CP_Empleado)
+    FOREIGN KEY (Cod_cliente) REFERENCES Cliente(Cod_cliente),
+    FOREIGN KEY (Cod_producto) REFERENCES Producto(Cod_producto),
+    FOREIGN KEY (Cod_Empleado) REFERENCES Empleados(Cod_Empleado),
+    PRIMARY KEY (Cod_Cliente, Cod_Producto, Cod_Empleado)
 );
 
 CREATE TABLE Almacén (
     Cod_almacén INT PRIMARY KEY,
-    CP_Producto INT,
-    FOREIGN KEY (CP_Producto) REFERENCES Producto(CP)
+    FOREIGN KEY (Cod_producto) REFERENCES Producto(Cod_producto)
 );
 
 CREATE TABLE Empleados (
-    CP INT PRIMARY KEY,
+    Cod_Empleado INT AUTOINCREMENT PRIMARY KEY,
     Nom_empleado VARCHAR(25),
     Ape_Empleado VARCHAR(25),
     DNI_Empleado VARCHAR(10),
-    Tlf_empleado INT
+    Tlf_empleado INT,
+    Clave_acceso VARCHAR(25)
 );
