fafec3a46f0e27df73ea3ba9ed148760343e1b1c
LauraS29 <lasalmo03@gmail.com>
2024-01-25 13:43:27 +0100

    .

--- 04_01 Desarrollo del sge/Db/Sql1.sql
+++ 04_01 Desarrollo del sge/Db/Sql1.sql
@@ -2,15 +2,16 @@ CREATE DATABASE IF NOT EXISTS trabajo;
 USE trabajo;
 
 CREATE TABLE Proveedores (
-    Cod_proveedor INT AUTOINCREMENT,
+    Cod_proveedor INT AUTO_INCREMENT PRIMARY KEY,
     Nom_proveedor VARCHAR(25),
     Email_proveedor VARCHAR(25),
-    Fecha_envio DATE
-    FOREIGN KEY (Cod_producto) REFERENCES Producto(Cod_producto)
+    Fecha_envio DATE,
+    Cod_producto INT,
+    FOREIGN KEY (Cod_producto) REFERENCES Producto(Cod_producto) 
 );
 
 CREATE TABLE Cliente (
-    Cod_cliente INT AUTOINCREMENT,
+    Cod_cliente INT AUTO_INCREMENT PRIMARY KEY,
     DNI_cliente VARCHAR(10),
     Nom_cliente VARCHAR(25),
     Ap_cliente VARCHAR(25),
@@ -19,30 +20,33 @@ CREATE TABLE Cliente (
     Provincia VARCHAR(25),
     Email_cliente VARCHAR(25),
     Tlf_cliente VARCHAR(25),
-    Observaciones VARCHAR(25),
+    Observaciones VARCHAR(25)
 );
 
 CREATE TABLE Producto (
-    Cod_producto INT PRIMARY KEY,
+    Cod_producto INT AUTO_INCREMENT PRIMARY KEY,
     Precio DECIMAL(10, 2),
     Cantidad INT
 );
 
 CREATE TABLE Tienda (
-    N_Stock INT,
+    Cod_cliente INT,
+    Cod_producto INT,
+    Cod_Empleado INT,
     FOREIGN KEY (Cod_cliente) REFERENCES Cliente(Cod_cliente),
     FOREIGN KEY (Cod_producto) REFERENCES Producto(Cod_producto),
     FOREIGN KEY (Cod_Empleado) REFERENCES Empleados(Cod_Empleado),
-    PRIMARY KEY (Cod_Cliente, Cod_Producto, Cod_Empleado)
+    PRIMARY KEY (Cod_cliente, Cod_producto, Cod_Empleado)
 );
 
 CREATE TABLE Almacén (
-    Cod_almacén INT PRIMARY KEY,
+    Cod_almacén INT AUTO_INCREMENT PRIMARY KEY,
+    Cod_producto INT,
     FOREIGN KEY (Cod_producto) REFERENCES Producto(Cod_producto)
 );
 
 CREATE TABLE Empleados (
-    Cod_Empleado INT AUTOINCREMENT PRIMARY KEY,
+    Cod_Empleado INT AUTO_INCREMENT PRIMARY KEY,
     Nom_empleado VARCHAR(25),
     Ape_Empleado VARCHAR(25),
     DNI_Empleado VARCHAR(10),
