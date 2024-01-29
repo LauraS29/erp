56a666151ad68d308e69d9941e841d424d05214b
2David0 <davidcriado122001@gmail.com>
2024-01-29 13:54:21 +0100

    .

--- 04_01 Desarrollo del sge/proveedores1.php
+++ 04_01 Desarrollo del sge/proveedores1.php
@@ -45,9 +45,17 @@ if (!$resultadoProveedores)
         </div>
     </header>
     <section class="fondo_section">
-        <div class="flex div1">
-            <img src="Assets/img/repartidor.png" alt="">
-            <p>Proveedores</p>
+        <div class="flex1">
+            <div class="flex">
+                <img src="Assets/img/repartidor.png" alt="">
+                <p>Proveedores</p>
+            </div>
+            <div>
+                <img src="Assets/img/ayudar.png" alt="">
+                <a href="Inicio.php">
+                    <img class="mg" src="Assets/img/cerrar-sesion.png" alt="">
+                </a>
+            </div>
         </div>
         <div class="fondo_div">
             <div>
@@ -55,26 +63,37 @@ if (!$resultadoProveedores)
             </div>
             <form class="fondo_form" action="proveedores1.php" method="post">
                 <table>
-                    <tr>
-                        <th>Código</th>
-                        <th>Proveedores</th>
-                        <th>Teléfono</th>
-                    </tr>
-
-                    <?php
-                    // Bucle para mostrar los datos de proveedores
-                    /* Verifica que $resultadoProveedores sea válido */
-                    while ($row = mysqli_fetch_assoc($resultadoProveedores)) 
-                    {
-                    ?>
+                    <div class="tabla">
                         <tr>
-                            <td><?php echo $row['Cod_proveedor']; ?></td>
-                            <td><a href="proveedores2.php?codigo=<?php echo $row['Cod_proveedor']; ?>&modo=editar"><?php echo $row['Nombre_proveedor']; ?></a></td>
-                            <td><?php echo $row['Telefono_proveedor']; ?></td>
-                        </tr>
-                    <?php
-                    }
-                    ?>
+                            <th>Código</th>
+                            <th>Proveedores</th>
+                            <th>Teléfono</th>
+                        </tr> 
+                    
+                        <?php
+                        // Bucle para mostrar los datos de proveedores
+                        /* Verifica que $resultadoProveedores sea válido */
+                        while ($row = mysqli_fetch_assoc($resultadoProveedores)) 
+                        {
+                        ?>
+                            <tr>
+                                <td><?php echo $row['Cod_proveedor']; ?></td>
+                                <td><a href="proveedores2.php?codigo=<?php echo $row['Cod_proveedor']; ?>&modo=editar"><?php echo $row['Nombre_proveedor']; ?></a></td>
+                                <td><?php echo $row['Telefono_proveedor']; ?></td>
+                                <td class="pequeño">
+                                    <div class="rect1">
+                                        <img src="Assets/img/actualizar.png" alt="Actualizar">
+                                    </div>
+                                    <div class="rect2">
+                                        <img src="Assets/img/eliminar.png" alt="Eliminar">
+                                    </div>
+                                </td>
+                            </tr>
+                        <?php
+                        }
+                        ?>
+                    </div>
+                    
                 </table>
                 <div class="button_prov">
                     <input type="submit" name="add_proveedor" value="Añadir">
