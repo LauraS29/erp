0ae155eb8c997f740089e2f1d4286aa47d3ec1a4
LauraS29 <lasalmo03@gmail.com>
2024-02-05 08:55:28 +0100

    .

---
+++ 04_01 Desarrollo del sge/Views/encabezado.php
@@ -0,0 +1,70 @@
+<header class="header1">
+        <div class="navegacion">
+            <ul>
+                <li>
+                    <a href="clientes1.php">
+                        <span class="flex alineado icono">
+                            <img src="Assets/img/clientes.png" alt="">
+                        </span>
+                        <span class="flex alineado titulo">Clientes</span>
+                    </a>
+                </li>    
+                <li>
+                    <a href="proveedores1.php">
+                        <span class="flex alineado icono">
+                            <img src="Assets/img/repartidor.png" alt="">
+                        </span>
+                        <span class="flex alineado titulo">Proveedores</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="personal1.php">
+                        <span class="flex alineado icono">
+                            <img src="Assets/img/personal.png" alt="">
+                        </span>
+                        <span class="flex alineado titulo">Personal</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="productos1.php">
+                        <span class="flex alineado icono">
+                            <img src="Assets/img/caja.png" alt="">
+                        </span>
+                        <span class="flex alineado titulo">Productos</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="ventas.php">
+                        <span class="flex alineado icono">
+                            <img src="Assets/img/venta.png" alt="">
+                        </span>
+                        <span class="flex alineado titulo">Ventas</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="compra1.php">
+                        <span class="flex alineado icono">
+                            <img src="Assets/img/compra.png" alt="">
+                        </span>
+                        <span class="flex alineado titulo negrita">Compra</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="pedido1.php">
+                        <span class="flex alineado icono">
+                            <img src="Assets/img/pedido.png" alt="">
+                        </span>
+                        <span class="flex alineado titulo">Pedidos</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="empresa1.php">
+                        <span class="flex alineado icono">
+                            <img src="Assets/img/calendario.png" alt="">
+                        </span>
+                        <span class="flex alineado titulo">Empresa</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </header>
