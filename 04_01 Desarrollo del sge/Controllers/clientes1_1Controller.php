f4c033865f937aa463b7a7fdabff44319f50d3a7
2David0 <davidcriado122001@gmail.com>
2024-02-20 10:19:52 +0100

    .

--- 04_01 Desarrollo del sge/Controllers/clientes1_1Controller.php
+++ 04_01 Desarrollo del sge/Controllers/clientes1_1Controller.php
@@ -6,6 +6,7 @@ require_once "Models/clientes1_1Model.php";
 
 // Tratamiento de los input text
 $Consulta1 = isset($_GET['Consulta1']) ? $_GET['Consulta1'] : '';
+$Cod_cliente = isset($_GET['Cod_cliente']) ? $_GET['Cod_cliente'] : '';
 
 // Instanciación de un objeto
 $oData = new Datos;
@@ -17,6 +18,16 @@ $rowsHTML = '';
 if (!empty($Consulta1)) {
     // Construir la consulta SQL para la búsqueda
     $sql = "SELECT Cod_cliente, Nom_cliente, DNI_cliente FROM cliente WHERE Cod_cliente = '$Consulta1' OR Nom_cliente = '$Consulta1' OR DNI_cliente = '$Consulta1' ORDER BY Cod_cliente, Nom_cliente, DNI_cliente";
+} else if (!empty($Cod_cliente)) {
+    // Obtener el nombre del cliente
+    $sql = "SELECT Nom_cliente FROM cliente WHERE Cod_cliente = '$Cod_cliente' LIMIT 1";
+    $result = $oData->getData1($sql);
+    if (!empty($result)) {
+        $client_name = $result[0]->Nom_cliente;
+    }
+
+    // Construir la consulta SQL para mostrar los datos del cliente
+    $sql = "SELECT Cod_cliente, Nom_cliente, DNI_cliente FROM cliente WHERE Cod_cliente = '$Cod_cliente' ORDER BY Cod_cliente, Nom_cliente, DNI_cliente";
 } else {
     // Construir la consulta SQL para obtener todos los datos
     $sql = "SELECT Cod_cliente, Nom_cliente, DNI_cliente FROM cliente ORDER BY Cod_cliente, Nom_cliente, DNI_cliente";
@@ -28,19 +39,20 @@ $data = $oData->getData1($sql);
 // Mostrar los resultados obtenidos
 foreach ($data as $row) {
     $rowsHTML .= "<tr>
-                    <td>{$row->Cod_cliente}</td>
-                    <td>{$row->Nom_cliente}</td>
-                    <td>{$row->DNI_cliente}</td>
-                    <td class='pequeño'>
-                        <a href='actualizar_fila.php?Cod_cliente=<?php echo $row->Cod_cliente; ?>'>
-                            <img src='Assets/img/actualizar.png' alt=''>
-                    </a>
-                        <a href='eliminar_fila.php?Cod_cliente={$row->Cod_cliente}'>
-                            <img class='img_elim' src='Assets/img/eliminar.png' alt=''>
-                        </a>
-                    </td>
-                  </tr>";
+        <td>{$row->Cod_cliente}</td>
+        <td>{$row->Nom_cliente}</td>
+        <td>{$row->DNI_cliente}</td>
+        <td class='pequeño'>
+            <a href='clientes2.php?Cod_cliente={$row->Cod_cliente}'>
+                <img src='Assets/img/actualizar.png' alt=''>
+            </a>
+            <a href='eliminar_fila.php?Cod_cliente={$row->Cod_cliente}'>
+                <img class='img_elim' src='Assets/img/eliminar.png' alt=''>
+            </a>
+        </td>
+    </tr>";
 }
+
 // Devolver las filas HTML
 echo $rowsHTML;
 ?>
--- 04_01 Desarrollo del sge/actualizar_fila.php
+++ 04_01 Desarrollo del sge/actualizar_fila.php
@@ -1,102 +1,33 @@
 <?php
-include 'Db/ConDb.php';
+session_start();
+include_once('Db/ConDb.php');
 
-$id = $_GET['Cod_cliente'];
-$sql = $mysqli->query('SELECT * FROM cliente WHERE Cod_cliente = $id');
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
 
+$Cod_cliente = $_GET['Cod_cliente'];
 
+// Actualizar la información del cliente
+
+$consulta = "UPDATE cliente SET 
+    Nom_cliente = '{$_POST['Nom_cliente']}',
+    Ap_cliente = '{$_POST['Ap_cliente']}',
+    Tlf_cliente = '{$_POST['Tlf_cliente']}',
+    Email_cliente = '{$_POST['Email_cliente']}',
+    DNI_cliente = '{$_POST['DNI_cliente']}',
+    Provincia = '{$_POST['Provincia']}',
+    Localidad = '{$_POST['Localidad']}'
+WHERE Cod_cliente = $Cod_cliente";
+
+$resultado = mysqli_query($conexion, $consulta);
+
+if (!$resultado) {
+    die("Error al actualizar: " . mysqli_error($conexion));
+}
+
+// Redirigir a clientes1.php
+header("Location: clientes1.php");
+exit();
 
 ?>
-
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Clientes</title>
-    <script src="./Assets/js/motor.js"></script>
-    <link rel="stylesheet" href="./Assets/css/estilos.css">
-</head>
-<body class = "flex">
-    <?php require_once "Views/Encabezado/Menu.php"; ?>
-    
-    <section class="fondo_section">
-        <div class="flex div1">
-            <img src="Assets/img/clientes.png" alt="">
-            <p class ="medio">Clientes</p>
-        </div>
-        <div class="fondo_div">
-            <div>
-                <h2>Datos de clientes</h2>
-            </div>
-            <form class="flex fondo_form" action="" method="post">
-                <?php 
-                    while ($datos=$sql->fetch_object()) {?>
-                        <div class="primer_div">
-                    <div class="flex">
-                        <div class="pr">
-                            <p>Nombre:</p>
-                            <input type="text" name="Nom_cliente" value="<?php $datos->$Nom_cliente ?>" required>
-                        </div>
-                        <div class="pr1">
-                            <p>Apellidos:</p>
-                            <input type="text" name="Ape_cliente" value="<?php $datos->$Ape_cliente?>" required>
-                        </div>
-                    </div>
-                    <div class="flex">
-                        <div class="pr">
-                            <p>Email:</p>
-                            <input type="email" name="Email_cliente" value="<?php $datos->$Email_cliente?>" required>
-                        </div>
-                        <div class="pr1">
-                            <p>Teléfono:</p>
-                            <input type="text" name="Tlf_cliente" value="<?php $datos->$Tlf_cliente?>" required>
-                        </div>
-                    </div>
-                    <div class="flex">
-                        <div class="pr">
-                            <p>DNI:</p>
-                            <input type="text" name="DNI_cliente" value="<?php echo $dato['DNI_cliente']; ?>" required>
-                        </div>
-                        <div class="pr1">
-                            <p>Codigo Postal:</p>
-                            <input type="text" name="Cod_postal_cliente" value="<?php echo $dato['Cod_postal_cliente']; ?>" required>
-                        </div>
-                    </div>
-                    <div class="flex">
-                        <div class="pr">
-                            <p>Localidad:</p>
-                            <input type="text" name="Localidad_cliente" value="<?php echo $dato['Localidad_cliente']; ?>" required>
-                        </div>
-                       <div class="pr1">
-                            <p>Dirección:</p>
-                            <input type="text" name="Provincia_cliente" value="<?php echo $dato['Provincia_cliente']; ?>" required>
-                        </div> 
-                    </div>
-                    <div class="flex">
-                        <div class="pr">
-                            <p>Observaciones:</p>
-                            <textarea name="Observaciones" value="<?php echo $dato['Observaciones']; ?>" cols="30" rows="10"></textarea>
-                        </div>
-                    </div>
-                </div>
-                 <?php }
-                ?>
-                <div class="segundo_div imagen-botones">
-                    <img src="Assets/img/usuario.png" alt="">
-                    <div class="buttons">
-                        <div>
-                            <input type="submit" name="guardar" id="boton1" value="Guardar">
-                        </div>
-                        <div>
-                            <button type="submit" name="guardar" id="boton1">Modificar</button>
-                        </div>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </section>
-</body>
-</html>
-
-<!-- https://www.youtube.com/watch?v=n6ZQOvvz19s -->
