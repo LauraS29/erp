a909d5fd08639430213cd960ab1cee538e36c332
LauraS29 <lasalmo03@gmail.com>
2024-02-28 13:48:13 +0100

    .

--- 04_01 Desarrollo del sge/Controllers/clientes1_1Controller.php
+++ 04_01 Desarrollo del sge/Controllers/clientes1_1Controller.php
@@ -30,9 +30,7 @@ foreach ($resultados as $fila) {
                 <input type='hidden' name='Cod_postal_cliente' value='" . $fila->Cod_postal_cliente . "'>
                 <input type='hidden' name='Localidad_cliente' value='" . $fila->Localidad_cliente . "'>
                 <input type='hidden' name='Provincia_cliente' value='" . $fila->Provincia_cliente . "'>
-                <button type='submit' name='eliminar' value='eliminar' onclick='return confirmacion()'>
-                    <img src='Assets/img/eliminar.png'>
-                </button>
+                <input type='submit' name='eliminar' value='eliminar' onclick='return confirmacion()'>
             </form>
           </td>";
 
@@ -48,12 +46,8 @@ foreach ($resultados as $fila) {
                 <input type='hidden' name='Cod_postal_cliente' value='" . $fila->Cod_postal_cliente . "'>
                 <input type='hidden' name='Localidad_cliente' value='" . $fila->Localidad_cliente . "'>
                 <input type='hidden' name='Provincia_cliente' value='" . $fila->Provincia_cliente . "'>
-                <input type='hidden' name='Observaciones' value='" . $fila->Observaciones . "'>
-
-                <button type='submit' name='editar'>
-                    <img src='Assets/img/actualizar.png'>
-                </button>
-
+                
+                <input type='submit' name='editar' value='editar'>
             </form>
           </td>";
 
@@ -62,7 +56,3 @@ foreach ($resultados as $fila) {
 
 
 ?>
-
-<!-- <a href='./actualizar_fila.php' name='editar'><img src='Assets/img/actualizar.png'></a> -->
-
-<!-- <input type='submit' name='editar' value='editar'> -->
